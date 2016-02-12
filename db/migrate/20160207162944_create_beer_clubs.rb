ae3f4bda0b09bc8dac2db68526860d18a47659c6
pundis <fuksi@dhcp-asv-106.helsinki.fi>
2016-02-12 15:29:52 +0200

    migration modifications for heroku

--- db/migrate/20160207162944_create_beer_clubs.rb
+++ db/migrate/20160207162944_create_beer_clubs.rb
@@ -2,8 +2,8 @@ class CreateBeerClubs < ActiveRecord::Migration
   def change
     create_table :beer_clubs do |t|
       t.string :name
-      t.integer :founded
       t.string :city
+      t.integer :founded
 
       t.timestamps null: false
     end
