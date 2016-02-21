e7413e36fb7c9af4f049c9e078a8b6c57581e469
pundis <fuksi@dhcp-asv-106.helsinki.fi>
2016-02-21 21:35:00 +0200

    week 4 1-10

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,5 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :beer_club
+  validates :beer_club_id, uniqueness: {scope: :user_id, message: "You're already a member of this beer club!"}
 end
